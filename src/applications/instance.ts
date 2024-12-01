73c42d95eca56d8e8c1e26884c258992193bcf6f
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-12-01 13:41:13 +0700

    fix: instance

--- src/applications/instance.ts
+++ src/applications/instance.ts
@@ -1,19 +1,13 @@
 import { Container } from "inversify";
 import { HttpClient } from "@/infrastructure/http/httpClient";
 import { TYPES } from "@/infrastructure/constants/types";
-import { AuthRepo } from "@/infrastructure/repositories/authRepo";
 import { AuthService } from "./services/authService";
-import { CategoryRepo } from "@/infrastructure/repositories/categoryRepo";
 import { CategoryService } from "./services/categoryService";
-import { LinkRepo } from "@/infrastructure/repositories/linkRepo";
 import { LinkService } from "./services/linkService";
 
 const container = new Container();
 
 container.bind(TYPES.httpClient).to(HttpClient);
-container.bind(TYPES.authRepo).to(AuthRepo);
-container.bind(TYPES.categoryRepo).to(CategoryRepo);
-container.bind(TYPES.linkRepo).to(LinkRepo);
 container.bind(AuthService).toSelf();
 container.bind(CategoryService).toSelf();
 container.bind(LinkService).toSelf();
