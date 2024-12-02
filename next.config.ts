c2070e02caef00e3645fcf53e1ee23c05eb3af3b
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-12-02 23:39:17 +0700

    fix: add allowed origin in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["url-manager.cloud", "localhost:3000"]
+    }
+  }
 };
 
 export default nextConfig;
