97dd8a844f76ee205bd0d4e7d223e313e14e4b4e
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-12-14 22:50:09 +0700

    fix: category list page

--- src/app/(main)/categories/page.tsx
+++ src/app/(main)/categories/page.tsx
@@ -1,11 +1,6 @@
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/app/_components/ui/table";
+  Card,
+} from "@/app/_components/ui/card";
 import { Search } from "@/app/_components/search";
 import dynamic from "next/dynamic";
 import { getCategories } from "./action";
@@ -40,27 +35,19 @@ export default async function Page(props: {
         <Search placeholder="Search categories..." query="name" />
         <AddCategory />
       </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {!res.success && <TableRow><TableCell colSpan={2} className="text-center">{res.message}</TableCell></TableRow>}
-          {res.success && res.data.length === 0 && <TableRow><TableCell colSpan={2} className="text-center">No data</TableCell></TableRow>}
-          {res.success && res.data.length > 0 && res.data.map((category) => (
-            <TableRow key={category.id}>
-              <TableCell className="font-medium">{category.name}</TableCell>
-              <TableCell className='space-x-3'>
-                <EditCategory name={category?.name} categoryId={category?.id} />
-                <DeleteCategory categoryId={category?.id} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <div className="grid grid-cols-4 gap-4">
+      {!res.success && <p>{res.message}</p>}
+      {res.success && res.data.length === 0 && <p>No data</p>}
+      {res.success && res.data.length > 0 && res.data.map(category => (
+        <Card key={category.id} className="p-3">
+          <p className="font-medium text-base">{category.name}</p>
+          <div className="space-x-2 mt-2">
+            <EditCategory name={category?.name} categoryId={category?.id} />
+            <DeleteCategory categoryId={category?.id} />
+          </div>
+        </Card>
+      ))}
+      </div>      
     </div>
   )
 }
