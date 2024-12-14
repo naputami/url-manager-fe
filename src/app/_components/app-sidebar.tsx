901ce3e51b4b369f0af66d0589ac0465cfe1fb67
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-12-14 22:48:08 +0700

    fix: logout method

--- src/app/_components/app-sidebar.tsx
+++ src/app/_components/app-sidebar.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { Tag, LinkIcon, LogOut, Loader2 } from "lucide-react"
+import { Tag, LinkIcon, LogOut, Loader2, MenuSquareIcon } from "lucide-react"
 import {
   Sidebar,
+  SidebarHeader,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
@@ -11,15 +12,23 @@ import {
   SidebarMenuItem,
   SidebarFooter
 } from "@/app/_components/ui/sidebar";
+import { Avatar, AvatarFallback } from "./ui/avatar";
 import { Button } from "./ui/button";
-import {logOutAction} from "@/app/(main)/action";
+import { logOutAction } from "@/app/(main)/action";
 import { useToast } from "@/hooks/use-toast";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { User } from "@/infrastructure/interfaces/entities";
+import { useProfileContext } from "@/context/profile";
 
 
 // Menu items.
 const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: MenuSquareIcon,
+  },
   {
     title: "Category",
     url: "/categories",
@@ -36,25 +45,50 @@ const items = [
 
 export function AppSidebar() {
   const [loading, setLoading] = useState(false);
-  const {toast} = useToast();
+  const { toast } = useToast();
+  const {setProfile, user} = useProfileContext();
+
+  useEffect(() => {
+    const userData = localStorage.getItem("user");
+    if (userData) {
+      const data: Partial<User> = JSON.parse(userData);
+      setProfile(data);
+    }
+  }, [])
+
+
   const handleLogOut = async () => {
     setLoading(true);
     const res = await logOutAction();
     if(!res){
       setLoading(false);
       toast({
-        title: "Login failed",
+        title: "Logout failed",
         description: "Try again later",
         variant: "destructive",
-    })
+    }) 
+    } else {
+      localStorage.removeItem("user");
+      setProfile({name: '', email: ''});
     }
 
   }
   return (
-    <Sidebar>
+    <Sidebar collapsible="offcanvas">
+      <SidebarHeader>
+        <div className="flex space-x-2 items-center">
+        <Avatar>
+          <AvatarFallback>{user?.name?.slice(0,1)}</AvatarFallback>
+        </Avatar>
+        <div>
+          <p className="text-lg font-semibold">{user?.name}</p>
+          <p className="text-sm font-light">{user?.email}</p>
+        </div>
+        </div>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
