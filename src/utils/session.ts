4663e0b6bda1b0013406408ab79b6ef64fb1efbc
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-12-14 22:43:46 +0700

    fix: change session chceking for redirect

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,12 +1,11 @@
+"use server";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function getSession(): Promise<string> {
-  const session = await cookies();
-  if (!session.toString()) {
-    console.log("no cookies");
+  const session = (await cookies()).get("session");
+  if (!session?.value) {
     redirect("/login");
   }
-
-  return session.toString();
+  return `session=${session.value}`;
 }
